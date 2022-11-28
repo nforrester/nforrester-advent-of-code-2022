dc96eee96550e61bc659d82fe3ce9d1730522564
Neil Forrester <neil@nforrester.net>
2022-11-28 11:49:59 -0800

    further prep

--- src/bin/advent-test.rs
+++ src/bin/advent-test.rs
@@ -4,9 +4,12 @@ use rusty_advent::*;
 
 fn main () {
     assert!(vec_char("hello")[4] == 'o');
+    assert!(vec_word("hello world")[1] == "world");
+    assert!(vec_by_sep("hello,world", ",")[1] == "world");
 
     assert!(file_lines("src/bin/advent-test.rs")[0] == "// test!");
-    assert!(file_vec_char_char("src/bin/advent-test.rs")[0][1] == '/');
+    assert!(file_vec_vec_char("src/bin/advent-test.rs")[0][1] == '/');
+    assert!(file_vec_vec_word("src/bin/advent-test.rs")[0][1] == "test!");
 
     let c = recap(r"^(?P<a>\S+) (?P<b>\S+) (?P<c>\S+)$", "hello 36 false");
     assert!(c.getstr("a") == "hello");
--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,47 @@ use std::vec::Vec;
 // string.split_whitespace()
 // string.split("separator")
 
+pub fn vec_word(string: &str) -> Vec<String> {
+    let mut ret = Vec::new();
+    for word in string.split_whitespace() {
+        ret.push(String::from(word));
+    }
+    return ret;
+}
+
 pub fn vec_char(string: &str) -> Vec<char> {
     Vec::from_iter(string.chars())
 }
 
+pub fn vec_by_sep(string: &str, sep: &str) -> Vec<String> {
+    let mut ret = Vec::new();
+    for word in string.split(sep) {
+        ret.push(String::from(word));
+    }
+    return ret;
+}
+
+pub fn file_string(filename: &str) -> String {
+    std::fs::read_to_string(filename).unwrap()
+}
+
 pub fn file_lines(filename: &str) -> Vec<String> {
     let mut ret = Vec::new();
-    for line in std::fs::read_to_string(filename).unwrap().lines() {
+    for line in file_string(filename).lines() {
         ret.push(String::from(line));
     }
     return ret;
 }
 
-pub fn file_vec_char_char(filename: &str) -> Vec<Vec<char>> {
+pub fn file_vec_vec_word(filename: &str) -> Vec<Vec<String>> {
+    let mut ret = Vec::new();
+    for line in file_lines(filename) {
+        ret.push(vec_word(line.as_str()));
+    }
+    return ret;
+}
+
+pub fn file_vec_vec_char(filename: &str) -> Vec<Vec<char>> {
     let mut ret = Vec::new();
     for line in file_lines(filename) {
         ret.push(vec_char(line.as_str()));
@@ -28,12 +56,6 @@ pub struct ReCap<'captures_lifetime> {
 }
 
 impl<'captures_lifetime> ReCap<'captures_lifetime> {
-    pub fn recap(re: &str, string: &'captures_lifetime str) -> ReCap<'captures_lifetime> {
-        ReCap {
-            captures: regex::Regex::new(re).unwrap().captures(string).unwrap(),
-        }
-    }
-
     pub fn getstr(&self, name: &str) -> &str {
         self.captures.name(name).unwrap().as_str()
     }
@@ -44,5 +66,7 @@ impl<'captures_lifetime> ReCap<'captures_lifetime> {
 }
 
 pub fn recap<'captures_lifetime>(re: &str, string: &'captures_lifetime str) -> ReCap<'captures_lifetime> {
-    ReCap::recap(re, string)
+    ReCap {
+        captures: regex::Regex::new(re).unwrap().captures(string).unwrap(),
+    }
 }
