1bef0d2581c2c1c3993de853c0dded7cbf9f2e9d
Neil Forrester <neil@nforrester.net>
2022-11-28 22:21:36 -0800

    2020 day 8 part2

--- src/bin/practice-2020d8.rs
+++ src/bin/practice-2020d8.rs
@@ -3,18 +3,16 @@ use rusty_advent::*;
 use std::collections::HashSet;
 use std::vec::Vec;
 
-fn main () {
-    let mut prog: Vec<(String, i64)> = Vec::new();
-    for line in file_vec_vec_word("input/practice-2020d8.txt") {
-        prog.push((line[0].clone(), line[1].parse().unwrap()));
-    }
-    let prog = prog;
-    let mut pc = 0;
+fn execute (prog: &Vec<(String, i64)>) -> Option<i64> {
+    let mut pc: i64 = 0;
     let mut acc = 0;
     let mut prev_pcs = HashSet::new();
     loop {
+        if pc >= prog.len() as i64 {
+            return Some(acc);
+        }
         if prev_pcs.contains(&pc) {
-            break;
+            return None;
         }
         prev_pcs.insert(pc);
         let op = &prog[pc as usize].0;
@@ -29,5 +27,29 @@ fn main () {
             pc += 1;
         }
     }
-    println!("{}", acc);
+}
+
+fn main () {
+    let mut prog: Vec<(String, i64)> = Vec::new();
+    for line in file_vec_vec_word("input/practice-2020d8.txt") {
+        prog.push((line[0].clone(), line[1].parse().unwrap()));
+    }
+    for idx in 0..prog.len() {
+        if prog[idx].0 == "jmp" {
+            prog[idx].0 = String::from("nop");
+        } else if prog[idx].0 == "nop" {
+            prog[idx].0 = String::from("jmp");
+        }
+
+        if let Some(acc) = execute(&prog) {
+            println!("{}", acc);
+            break;
+        }
+
+        if prog[idx].0 == "jmp" {
+            prog[idx].0 = String::from("nop");
+        } else if prog[idx].0 == "nop" {
+            prog[idx].0 = String::from("jmp");
+        }
+    }
 }
