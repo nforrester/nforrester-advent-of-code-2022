770f688df2ee57b9151926a05876d356ee184f57
Neil Forrester <neil@nforrester.net>
2022-12-09 21:47:26 -0800

    Day 10 second draft

--- src/bin/d10.rs
+++ src/bin/d10.rs
@@ -1,97 +1,62 @@
 use rusty_advent::*;
 
-//use std::collections::HashMap;
-//use std::collections::HashSet;
-//use std::cmp::max;
-//use std::cmp::min;
-
-fn part1(filename: &str) {
-    let mut answer = 0;
-    let mut x: i64 = 1;
-    let mut cycle = 0;
-    for words in file_vec_vec_word(filename) {
-        if words[0] == "noop"
-        {
-            cycle += 1;
-            if (cycle - 20) % 40 == 0 {
-                let y = cycle * x;
-                answer += y;
-            }
-        }
-        else
-        {
-            cycle += 1;
-            if (cycle - 20) % 40 == 0 {
-                let y = cycle * x;
-                answer += y;
-            }
-            cycle += 1;
-            if (cycle - 20) % 40 == 0 {
-                let y = cycle * x;
-                answer += y;
-            }
-            x += words[1].parse::<i64>().unwrap();
-        }
-    }
-    println!("{}", answer);
-    assert_eq!(answer, 15260);
+struct State {
+    x: i64,
+    cycle: i64,
+    signal_strength_total: i64,
+    display: String,
 }
 
-fn part2(filename: &str) {
-    let mut answer = "\n".to_string();
-    let mut x: i64 = 1;
-    let mut pix = 0;
-    for words in file_vec_vec_word(filename) {
+impl State {
+    fn next_cycle(&mut self) {
+        if self.cycle % 40 == 0 {
+            self.display.push('\n');
+        }
+
+        if (self.x - self.cycle % 40).abs() <= 1 {
+            self.display.push('#');
+        } else {
+            self.display.push('.');
+        }
+
+        self.cycle += 1;
+
+        if (self.cycle - 20) % 40 == 0 {
+            self.signal_strength_total += self.cycle * self.x;
+        }
+    }
+}
+
+fn main() {
+    let mut state = State {
+            x: 1,
+            cycle: 0,
+            signal_strength_total: 0,
+            display: String::new(),
+        };
+
+    for words in file_vec_vec_word("input/d10.txt") {
         if words[0] == "noop"
         {
-            if (x-pix).abs() <= 1 {
-                answer.push('#');
-            } else {
-                answer.push('.');
-            }
-            pix += 1;
-            if pix == 40 {
-                pix = 0;
-                answer.push('\n');
-            }
+            state.next_cycle();
         }
         else
         {
-            if (x-pix).abs() <= 1 {
-                answer.push('#');
-            } else {
-                answer.push('.');
-            }
-            pix += 1;
-            if pix == 40 {
-                pix = 0;
-                answer.push('\n');
-            }
-            if (x-pix).abs() <= 1 {
-                answer.push('#');
-            } else {
-                answer.push('.');
-            }
-            pix += 1;
-            if pix == 40 {
-                pix = 0;
-                answer.push('\n');
-            }
-            x += words[1].parse::<i64>().unwrap();
+            state.next_cycle();
+            state.next_cycle();
+            state.x += words[1].parse::<i64>().unwrap();
         }
     }
-    println!("{}", answer);
-    assert_eq!(answer, "
+
+    println!("{}", state.signal_strength_total);
+    assert_eq!(state.signal_strength_total, 15260);
+
+    println!("{}", state.display);
+    assert_eq!(state.display, "
 ###...##..#..#.####..##..#....#..#..##..
 #..#.#..#.#..#.#....#..#.#....#..#.#..#.
 #..#.#....####.###..#....#....#..#.#....
 ###..#.##.#..#.#....#.##.#....#..#.#.##.
 #....#..#.#..#.#....#..#.#....#..#.#..#.
-#.....###.#..#.#.....###.####..##...###.
-");
-}
-
-fn main() {
-    part1("input/d10.txt");
-    part2("input/d10.txt");
+#.....###.#..#.#.....###.####..##...###.");
 }
